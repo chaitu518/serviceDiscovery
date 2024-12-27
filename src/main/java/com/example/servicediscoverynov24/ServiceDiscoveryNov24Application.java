3dc869c4bd3b901c046a4ee76c2e587def487b81
saichaitanya518 <saichaitanya518@gmail.com>
2024-12-27 15:37:30 +0530

    first commit

---
+++ src/main/java/com/example/servicediscoverynov24/ServiceDiscoveryNov24Application.java
@@ -0,0 +1,13 @@
+package com.example.servicediscoverynov24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryNov24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryNov24Application.class, args);
+	}
+
+}
